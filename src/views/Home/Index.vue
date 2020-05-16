0e9b4aef843e0af1492da09a9930db7c4f554018
Estauti <estautib@gmail.com>
2020-05-16 19:43:35 -0300

    Remove a linter rule and finished Alerts table

--- src/views/Home/Index.vue
+++ src/views/Home/Index.vue
@@ -1,13 +1,31 @@
 <template>
   <div>
-    <h1>Últimos alertas</h1>
-    <ul>
-      <li v-for="alert in alerts" :key="alert.id">
-        {{ alert.device_name }}
-        {{ $moment(alert.started_at).format("DD/MM/YYYY HH:mm:ss") }}
-        {{ $moment(alert.finished_at).format("DD/MM/YYYY HH:mm:ss") }}
-      </li>
-    </ul>
+    <b-row>
+      <b-col cols=6 class="col-r-border">
+        <h1>Últimos alertas</h1>
+        <b-table 
+          striped 
+          hover 
+          :items="alerts"
+          :fields="alerts_fields"
+          class="mt-4"
+        >
+
+        <template v-slot:cell(finished_at)="row">
+          <span v-if="row.value == 'Invalid date'">
+            <font-awesome-icon 
+              icon="exclamation-triangle"
+              size="lg" 
+              class="text-danger"
+            />
+            Aberta
+          </span>
+          <span v-else>{{ row.value }}</span>
+        </template>
+
+        </b-table>
+      </b-col>
+    </b-row>
   </div>
 </template>
 
@@ -17,15 +35,50 @@ import { api_url } from '@/helpers/api_url'
 export default {
   data() {
     return {
-      alerts: []
+      device_search_id: null,
+      alerts: [],
+      alerts_fields: [
+        {
+          key: 'device_name',
+          label: "Dispositivo"
+        },
+        {
+          key: 'started_at',
+          label: 'Início',
+          formatter: (value) => {
+            return this.$moment(value).format("DD/MM/YYYY HH:mm:ss")
+          }
+        },
+        {
+          key: 'finished_at',
+          label: 'Encerrado',
+          formatter: (value) => {
+            return this.$moment(value).format("DD/MM/YYYY HH:mm:ss")
+          }
+        },
+        {
+          key: 'duration',
+          label: 'Duração',
+          formatter: (value, key, item) => {
+            let duration = this.$moment(item.finished_at).from(this.$moment(item.started_at), true);
+
+            if (duration == 'Invalid date') {
+              return '--';
+            }
+            return duration;
+          }
+        }
+      ]
     }
   },
   mounted() {
-    this.getDeviceAlerts(2);
+    this.getAlerts();
   },
   methods: {
-    getDeviceAlerts(device_id) {
-      this.axios.get(`${api_url}/api/alerts.json`, {device_id})
+    getAlerts() {
+      this.axios.get(`${api_url}/api/alerts.json`, {
+        device_id: this.device_search_id
+      })
         .then(response => {
           this.alerts = response.data;
         });
@@ -35,5 +88,7 @@ export default {
 </script>
 
 <style>
-
+  .col-r-border {
+    border-right: 1px solid #c3c2c2;
+  }
 </style>
